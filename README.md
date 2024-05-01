35d0228693adb747c4d1eee2359561c8f31ccfe8
Amirata Ghorbani <info@amiratag.com>
2024-05-01 09:09:57 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# ACE
+
+ACE: Towards Automatic Concept Based Explanations
+
+**Please cite the following work if you use this benchmark or the provided tools or implementations:**
+```
+@inproceedings{ghorbani2019towards,
+  title={Towards automatic concept-based explanations},
+  author={Ghorbani, Amirata and Wexler, James and Zou, James Y and Kim, Been},
+  booktitle={Advances in Neural Information Processing Systems},
+  pages={9273--9282},
+  year={2019}
+}
+```
+## Getting Started
+Here is the tensorflow implementations of the paper [Towards Automatic Concept-based Explanations](https://arxiv.org/abs/1902.03129) presented at NeurIPS 2019.
+
+```
+Ghorbani, Amirata, James Wexler, James Y. Zou, and Been Kim. 
+"Towards Automatic Concept-based Explanations." 
+Advances in Neural Information Processing Systems. 2019.
+````
+### Prerequisites
+
+Required python libraries:
+
+```
+  Scikit-image: https://scikit-image.org/
+  Tensorflow: https://www.tensorflow.org/
+  TCAV: https://github.com/tensorflow/tcav
+```
+
+### Installing
+
+An example run command:
+
+```
+python3 ace_run.py --num_parallel_runs 0 --target_class zebra --source_dir SOURCE_DIR --working_dir SAVE_DIR --model_to_run GoogleNet --model_path ./tensorflow_inception_graph.pb --labels_path ./imagenet_labels.txt --bottlenecks mixed4c --num_random_exp 40 --max_imgs 50 --min_imgs 30
+```
+
+where:
+```
+num_random_exp: number of random concepts with respect to which concept-activaion-vectors are computed for calculating the TCAV score of a discovered concept (recommended >20).
+```
+For example if you set num_random_exp=20, you need to create folders random500_0, rando500_1, ..., random_500_19 and put them in the SOURCE_DIR where each folder contains a set of 50-500 randomly selected images of the dataset (ImageNet in our case). 
+
+```
+target_class: Name of the class which prediction is to be explained.
+```
+
+```
+SOURCE_DIR: Directory where the discovery images (refer to the paper) are saved. 
+It should contain (at least) num_random_exp + 2 folders: 
+1-"target_class" which contains images of the class to be explained (in this example the shoulder should be names as zebra). 
+2-"random_discovery" which contains randomly selected images of the same dataset (at lease $max_imgs number of images).
+3-"random500_0, ..., random_500_${num_random_exp} where each one contains 500 randomly selected images from the data set"
+```
+
+```
+num_parallel_runs: Number of parallel jobs (loading images, etc). If 0, parallel processing is deactivated.
+```
+
+
+```
+SAVE_DIR: Where the experiment results (both text report and the discovered concept examples) are saved.
+```
+
+```
+model_to_run: One of InceptionV3 or GoogleNet is supported (the weights are provided for GoogleNet). You can change the "make_model" function in ace_helpers.py to have your own customized model.
+model_path: Path to the model's saved graph.
+```
+If you are using a custom model, you should write a wrapper for it containing the following methods:
+```
+run_examples(images, BOTTLENECK_LAYER): which basically returens the activations of the images in the BOTTLENECK_LAYER. 'images' are original images without preprocessing (float between 0 and 1)
+get_image_shape(): returns the shape of the model's input
+label_to_id(CLASS_NAME): returns the id of the given class name.
+get_gradient(activations, CLASS_ID, BOTTLENECK_LAYER): computes the gradient of the CLASS_ID logit in the logit layer with respect to activations in the BOTTLENECK_LAYER.
+```
+
+## Authors
+
+* **Amirata Ghorbani** - [Website](http://web.stanford.edu/~amiratag)
+* **James Wexler** - [Website](https://ai.google/research/people/105507/)
+* **James Zou** - [Website](https://www.james-zou.com/)
+* **Been Kim** - [Website](https://beenkim.github.io/)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+Work was done as part of Google Brain internship.
+
---
+++ ace_run.py
@@ -0,0 +1,113 @@
+"""This script runs the whole ACE method."""
+
+
+import sys
+import os
+import numpy as np
+import sklearn.metrics as metrics
+from tcav import utils
+import tensorflow as tf
+
+import ace_helpers
+from ace import ConceptDiscovery
+import argparse
+
+
+def main(args):
+
+  ###### related DIRs on CNS to store results #######
+  discovered_concepts_dir = os.path.join(args.working_dir, 'concepts/')
+  results_dir = os.path.join(args.working_dir, 'results/')
+  cavs_dir = os.path.join(args.working_dir, 'cavs/')
+  activations_dir = os.path.join(args.working_dir, 'acts/')
+  results_summaries_dir = os.path.join(args.working_dir, 'results_summaries/')
+  if tf.gfile.Exists(args.working_dir):
+    tf.gfile.DeleteRecursively(args.working_dir)
+  tf.gfile.MakeDirs(args.working_dir)
+  tf.gfile.MakeDirs(discovered_concepts_dir)
+  tf.gfile.MakeDirs(results_dir)
+  tf.gfile.MakeDirs(cavs_dir)
+  tf.gfile.MakeDirs(activations_dir)
+  tf.gfile.MakeDirs(results_summaries_dir)
+  random_concept = 'random_discovery'  # Random concept for statistical testing
+  sess = utils.create_session()
+  mymodel = ace_helpers.make_model(
+      sess, args.model_to_run, args.model_path, args.labels_path)
+  # Creating the ConceptDiscovery class instance
+  cd = ConceptDiscovery(
+      mymodel,
+      args.target_class,
+      random_concept,
+      args.bottlenecks.split(','),
+      sess,
+      args.source_dir,
+      activations_dir,
+      cavs_dir,
+      num_random_exp=args.num_random_exp,
+      channel_mean=True,
+      max_imgs=args.max_imgs,
+      min_imgs=args.min_imgs,
+      num_discovery_imgs=args.max_imgs,
+      num_workers=args.num_parallel_workers)
+  # Creating the dataset of image patches
+  cd.create_patches(param_dict={'n_segments': [15, 50, 80]})
+  # Saving the concept discovery target class images
+  image_dir = os.path.join(discovered_concepts_dir, 'images')
+  tf.gfile.MakeDirs(image_dir)
+  ace_helpers.save_images(image_dir,
+                            (cd.discovery_images * 256).astype(np.uint8))
+  # Discovering Concepts
+  cd.discover_concepts(method='KM', param_dicts={'n_clusters': 25})
+  del cd.dataset  # Free memory
+  del cd.image_numbers
+  del cd.patches
+  # Save discovered concept images (resized and original sized)
+  ace_helpers.save_concepts(cd, discovered_concepts_dir)
+  # Calculating CAVs and TCAV scores
+  cav_accuraciess = cd.cavs(min_acc=0.0)
+  scores = cd.tcavs(test=False)
+  ace_helpers.save_ace_report(cd, cav_accuraciess, scores,
+                                 results_summaries_dir + 'ace_results.txt')
+  # Plot examples of discovered concepts
+  for bn in cd.bottlenecks:
+    ace_helpers.plot_concepts(cd, bn, 10, address=results_dir)
+  # Delete concepts that don't pass statistical testing
+  cd.test_and_remove_concepts(scores)
+
+def parse_arguments(argv):
+  """Parses the arguments passed to the run.py script."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--source_dir', type=str,
+      help='''Directory where the network's classes image folders and random
+      concept folders are saved.''', default='./ImageNet')
+  parser.add_argument('--working_dir', type=str,
+      help='Directory to save the results.', default='./ACE')
+  parser.add_argument('--model_to_run', type=str,
+      help='The name of the model.', default='GoogleNet')
+  parser.add_argument('--model_path', type=str,
+      help='Path to model checkpoints.', default='./tensorflow_inception_graph.pb')
+  parser.add_argument('--labels_path', type=str,
+      help='Path to model checkpoints.', default='./imagenet_labels.txt')
+  parser.add_argument('--target_class', type=str,
+      help='The name of the target class to be interpreted', default='zebra')
+  parser.add_argument('--bottlenecks', type=str,
+      help='Names of the target layers of the network (comma separated)',
+                      default='mixed4c')
+  parser.add_argument('--num_random_exp', type=int,
+      help="Number of random experiments used for statistical testing, etc",
+                      default=20)
+  parser.add_argument('--max_imgs', type=int,
+      help="Maximum number of images in a discovered concept",
+                      default=40)
+  parser.add_argument('--min_imgs', type=int,
+      help="Minimum number of images in a discovered concept",
+                      default=40)
+  parser.add_argument('--num_parallel_workers', type=int,
+      help="Number of parallel jobs.",
+                      default=0)
+  return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
+
